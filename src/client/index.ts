3174e71ef93826db22de1f888548990040539455
Kodai-Yamamoto-SIW <k-yamamoto@siw.ac.jp>
2025-09-29 17:02:48 +0900

    feat: add exports for types and modules in package.json and create client index file

---
+++ src/client/index.ts
@@ -0,0 +1,21 @@
+export {
+  AuthAccountProvider,
+  useAuthAccountContext,
+  type AuthAccountContextValue,
+} from './AuthAccountContext';
+
+export {
+  resolveAuthConfig,
+  buildMsalConfiguration,
+  DEFAULT_SCOPES,
+  type AuthConfig,
+  type EnabledAuthConfig,
+  type DisabledAuthConfig,
+  type AuthEnvironment,
+} from './authConfig';
+
+export {
+  getMsalInstance,
+  createLoginRequest,
+  createLogoutRequest,
+} from './msalClient';
